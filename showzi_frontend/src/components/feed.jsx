9f6b04095e9ce769b71c77232c5510343cfd6a22
varshalohakare <lohakarevarsha43@gmail.com>
2026-02-15 17:02:17 +0530

    Fix authentication and add logout feature-2

--- showzi_frontend/src/components/feed.jsx
+++ showzi_frontend/src/components/feed.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { data, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import { client } from "../client";
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
--- showzi_frontend/src/components/navbar.jsx
+++ showzi_frontend/src/components/navbar.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { IoMdAdd, IoMdSearch } from "react-icons/io";
-import { googleLogout } from "@react-oauth/google";
 
 const Navbar = ({ SearchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
-  // const handleLogout = () => {
-  //   googleLogout();
-  //   localStorage.clear();
-  //   navigate("/login"); // Redirect to login page after logout
-  //   // Reload the page after logout
-  // };
-
-  const [showDropdown, setShowDropdown] = React.useState(false);
 
   const handleCreatePinRoute = () => {
     navigate("/create-pin");
   };
 
   const handleUserRoute = () => {
-    navigate(`/user-profile/${user?._id}`)
-    setShowDropdown((prev) => !prev)
+    navigate(`/user-profile/${user?._id}`);
   };
 
   return (
@@ -36,8 +26,8 @@ const Navbar = ({ SearchTerm, setSearchTerm, user }) => {
           onFocus={() => navigate("/Search")}
           className="p-2 w-full bg-white outline-none"
         />
-        <div className="flex gap-3 items-center justify-center">
 
+        <div className="flex gap-3 items-center justify-center">
           <button onClick={handleUserRoute} className="hidden md:block">
             <img
               src={user?.image}
@@ -45,20 +35,10 @@ const Navbar = ({ SearchTerm, setSearchTerm, user }) => {
               className="w-14 h-12 rounded-lg"
             />
           </button>
-          {/* {showDropdown && (
-            <div className="absolute  top-16 right-28 mt-2 w-30 bg-white shadow-lg rounded-lg z-20">
-              <button
-                className="w-full text-center px-4 py-2 text-gray-700 hover:bg-red-100 "
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </div>
-          )} */}
-          
+
           <button
             onClick={handleCreatePinRoute}
-            className="bg-black text-white rounded-lg w-12 h-12 md-14 md:h-12 flex justify-center  items-center"
+            className="bg-black text-white rounded-lg w-12 h-12 md:h-12 flex justify-center items-center"
           >
             <IoMdAdd />
           </button>
